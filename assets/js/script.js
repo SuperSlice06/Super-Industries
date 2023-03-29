12f341780e22351b1d2d441126d4604ba61130bb
SuperSlice06 <seanlp06@live.com>
2023-03-29 00:21:08 -0700

    Language Support + Dark/Light Theme

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,30 +77,84 @@ addEventListener('resize', removeStyle)
 
 /*=============== LIGHT AND DARK MODE ===============*/
 
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-const currentTheme = localStorage.getItem('theme');
+function themeSwitch() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
 
-if (currentTheme) {
-document.documentElement.setAttribute('data-theme', currentTheme);
 
-if (currentTheme === 'dark') {
-toggleSwitch.checked = true;
-document.documentElement.classList.add('dark-mode');
-}
-} else {
-document.documentElement.setAttribute('data-theme', 'light');
-}
+        const themeIcon = document.getElementById("theme-icon");
 
-function switchTheme(e) {
-if (e.target.checked) {
-document.documentElement.setAttribute('data-theme', 'dark');
-localStorage.setItem('theme', 'dark');
-document.documentElement.classList.add('dark-mode');
-} else {
-document.documentElement.setAttribute('data-theme', 'light');
-localStorage.setItem('theme', 'light');
-document.documentElement.classList.remove('dark-mode');
-}
+            if(themeIcon.classList.contains("ri-sun-fill")){
+                            themeIcon.classList.remove("ri-sun-fill")
+              themeIcon.classList.add("ri-moon-fill")
+            } else {
+              themeIcon.classList.add("ri-sun-fill")
+
+            }
+  
 }
 
-toggleSwitch.addEventListener('change', switchTheme, false);
+
+/*=============== TRANSLATE ENGLISH AND SPANISH ===============*/
+
+const langZH = {
+  // Navbar
+  'nav_logo': '超级工业',
+  'nav_toggle_menu': '菜单',
+  'nav_toggle_close': '关闭',
+  'nav_link_about': '关于',
+  'nav_dropdown_resources': '资源',
+  'nav_dropdown_about_us': '关于我们',
+  'nav_dropdown_support': '支持',
+  'nav_dropdown_contact_us': '联系我们',
+  'nav_dropdown_safety': '安全和质量',
+  'nav_dropdown_cookie': 'Cookie设置',
+  'nav_dropdown_privacy': '隐私政策',
+
+  // Main Content
+  'main_title': '欢迎来到超级工业！',
+  'main_text': '我们是一家领先的技术和创新公司。我们的目标是创建尖端的产品，满足客户的需求并超越他们的期望。联系我们以了解更多关于我们的服务。'
+};
+
+const langzhES = {
+  "Welcome to Super Industries!": "¡Bienvenidos a Super Industries!",
+  "We are a leading company in the field of technology and innovation. Our goal is to create cutting-edge products that meet the needs of our customers and exceed their expectations. Contact us to learn more about our services.": "Somos una empresa líder en el campo de la tecnología y la innovación. Nuestro objetivo es crear productos de vanguardia que satisfagan las necesidades de nuestros clientes y superen sus expectativas. Contáctanos para obtener más información sobre nuestros servicios.",
+  "About": "Sobre nosotros",
+  "Resources": "Recursos",
+  "About us": "Sobre nosotros",
+  "Support": "Soporte",
+  "Contact us": "Contáctanos",
+  "Safety and quality": "Seguridad y calidad",
+  "Cookie settings": "Configuración de cookies",
+  "Privacy Policy": "Política de privacidad",
+
+};
+
+function setLanguage() {
+
+
+  
+  localStorage.setItem('lang', lang);
+  updateLanguage();
+}
+
+function updateLanguage() {
+  const lang = localStorage.getItem('lang') || 'en';
+  const langFile = `./lang_${lang}.js`;
+  fetch(langFile)
+    .then(response => response.json())
+    .then(data => {
+      // Update Navbar
+      document.querySelector('.nav__logo').innerHTML = `<i class="ri-code-s-slash-line light-mode"></i> ${data['nav_logo']}`;
+      document.querySelector('.nav__toggle-menu').innerHTML = data['nav_toggle_menu'];
+      document.querySelector('.nav__toggle-close').innerHTML = data['nav_toggle_close'];
+      document.querySelector('#trial').innerHTML = data['nav_link_about'];
+      document.querySelector('.dropdown__button').innerHTML = `${data['nav_dropdown_resources']} <i class="ri-arrow-down-s-line dropdown__arrow"></i>`;
+      document.querySelector('.dropdown__title:nth-of-type(1)').innerHTML = data['nav_dropdown_about_us'];
+      document.querySelector('.dropdown__link:nth-of-type(1)').innerHTML = data['nav_dropdown_about_us'];
+      document.querySelector('.dropdown__link:nth-of-type(2)').innerHTML = data['nav_dropdown_support'];
+      document.querySelector('.dropdown__link:nth-of-type(3)').innerHTML = data['nav_dropdown_contact_us'];
+      document.querySelector('.dropdown__title:nth-of-type(2)').innerHTML = data['nav_dropdown_safety'];
+      document.querySelector('.dropdown__link:nth-of-type(4)').innerHTML = data['nav_dropdown_cookie'];
+      document.querySelector('.dropdown__link:nth-of-type(5)').innerHTML = data['nav_dropdown_privacy'];
+    })}
